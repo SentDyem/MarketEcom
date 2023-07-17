1a50a471107a952ab3e23fa24658ece1b0abdb48
SentDyem <55694826+SentDyem@users.noreply.github.com>
2023-07-17 22:32:10 +0300

    set productdetail page, add redux package

--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 
@@ -7,9 +7,16 @@ const ProductDetail = () => {
     const route = useRoute()
   return (
     <View style = {styles.container}>
-      <Text>ProductDetail</Text>
-      <Image source={{uri:route.params.data._data.productImage}} />
-      <Text>{route.params.data._data.price}</Text>
+        <View style = {{justifyContent:'center', alignItems:'center'}}>
+        <Image style = {styles.productImage} source={{uri:route.params.data._data.productImage}} />
+      <Text>{route.params.data._data.productName}</Text>
+      <Text>{route.params.data._data.productDesc}</Text>
+      <Text>{route.params.data._data.price}</Text>      
+        </View>
+      
+      <TouchableOpacity style={styles.addNewBtn} onPress={() => {  }}>
+        <Text style={styles.btnText}>В корзину</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -17,5 +24,29 @@ const ProductDetail = () => {
 export default ProductDetail
 
 const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    productImage: {
+        width: '100%',
+        height: 300,
+        resizeMode:'center'
+    },
+    addNewBtn: {
+        backgroundColor: 'blue',
+        height: 50,
+        width: '80%',
+        position: 'absolute',
+        borderRadius: 10,
+        alignItems: 'center',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        bottom: 10
+      },
+      btnText: {
+        color: 'white',
+        fontSize: 16,
+    
+      },
 
 })
